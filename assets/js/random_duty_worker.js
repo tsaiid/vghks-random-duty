a7a623442ac7b146c002325bbfb789fafa862ff4
I-Ta Tsai@MBPR <ittsai@gmail.com>
2015-07-18 19:26:55 +0800

    do not log qod info now.

--- assets/js/random_duty_worker.js
+++ assets/js/random_duty_worker.js
@@ -87,7 +87,7 @@ function less_than_qod_times(group_duties, qod_limit) {
     for (var person in group_duties) {
         var qod_times = group_duties[person].intervals.multiIndexOf(2).length;
         if (qod_times > parseInt(qod_limit)) {
-            console.log("qod_times: " + qod_times + ", intervals: " + group_duties[person].intervals.toString());
+            // console.log("qod_times: " + qod_times + ", intervals: " + group_duties[person].intervals.toString());
             return false;
         }
     }
